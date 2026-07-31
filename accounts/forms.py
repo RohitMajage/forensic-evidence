86db1f481f95f207e4cdcbc475a1b5665043cdd6
Rohit Majage <rohitmajage1062@gmail.com>
2026-07-31 23:53:15 +0530

    Fix syntax error and form fields in CustomUserCreationForm for valid registration

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,10 +2,12 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import *
 
+
+
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ['username', 'email', 'password1', 'password2', 'phone_number']
+        fields = ['username', 'email', 'phone_number']
         widgets = {
             'username': forms.TextInput(attrs={
                 'class': 'form-control',
@@ -20,6 +22,7 @@ class CustomUserCreationForm(UserCreationForm):
                 'placeholder': 'Enter your phone number',
             }),
         }
+
 
     # Override the default widgets for password fields
     def __init__(self, *args, **kwargs):
