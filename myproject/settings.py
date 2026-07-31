8af4076a06dc98651ce022ad9b4aac92a7389590
Rohit Majage <rohitmajage1062@gmail.com>
2026-08-01 00:32:09 +0530

    Fix: always use SMTP backend so OTP emails are actually sent

--- myproject/settings.py
+++ myproject/settings.py
@@ -144,15 +144,10 @@ LOGOUT_REDIRECT_URL = 'login'
 
 
 # Email configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'rohitmajage1062@gmail.com')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'drik hofn ljcw ukhv')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-
-# Use SMTP on production (when EMAIL_HOST_PASSWORD env var is set), console locally
-if os.environ.get('EMAIL_HOST_PASSWORD'):
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-else:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
